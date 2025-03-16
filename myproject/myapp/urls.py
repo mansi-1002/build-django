677724bec82b89bb4ae40b5271c16bdb63aa8dab
mansi-1002 <mansismane2005@gmail.com>
2025-03-16 14:28:08 +0530

    add menu and year

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from . import views 
+from . import views
 
 urlpatterns = [
-    path('', views.home ), 
+    path('hello/', views.hello, name='hello'),
+    path('homepage/', views.homepage, name='homepage'),
+    path('display_date/', views.display_date, name='display_date'),
+    path('menu/', views.menu, name='menu'),
 ]
 
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,4 +1,27 @@
 from django.http import HttpResponse
 
-def home(request):
-    return HttpResponse("Welcome to the Little Lemon restaurant")
+def hello(request):
+    return HttpResponse('Hello World')
+def homepage(request):
+    return HttpResponse('Welcome to Little Lemon')
+from datetime import datetime
+
+def display_date(request):
+    date_joined = datetime.now().year
+    return HttpResponse(f'This year is {date_joined}')
+def menu(request):
+    html_code = """
+    <html>
+    <head>
+        <style>
+            body { color: green; }
+            h1 { color: blue; }
+        </style>
+    </head>
+    <body>
+        <h1>Menu</h1>
+        <p>Welcome to the menu page!</p>
+    </body>
+    </html>
+    """
+    return HttpResponse(html_code)
